8311e7606df177d58b27ea8f331519d9b5aad19a
mansurbek <ergashovmansurbek5577@gmail.com>
2025-09-10 11:07:00 +0500

    no-design

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const elCardTemplate = document.getElementById("cardTemplate");
+const elCardTemplate = document.getElementById("cardTemp");
 const elContainer = document.getElementById("container");
 const elLoading = document.getElementById("loading");
-const elError = document.getElementById("error");
+// const elError = document.getElementById("error");
 
 
 
@@ -11,25 +11,31 @@ fetch("https://json-api.uz/api/project/fn43/cars")
         return res.json();
     })
     .then((res) => {
-        ui(res);
+        ui(res.data);
     })
     .catch(() => {
         console.log("Xatolik");
-        elError.classList.remove("hidden");
+        // elError.classList.remove("hidden");
     })
     .finally(() => {
         elLoading.remove()
     })
 
-    
-function ui(todos) {
-    todos.forEach(element => {
+
+function ui(cars) {
+    cars.forEach(element => {
         const clone = elCardTemplate.cloneNode(true).content;
         const elTitle = clone.querySelector("h2");
-        const elButton = clone.querySelector("button");
+        const elBrand = clone.querySelector("h3");
+        const elDescription = clone.querySelector("p");
+        const elCategory = clone.querySelector("mark");
+        const elPrice = clone.querySelector("i");
 
-        elTitle.innerText = element.title;
-        elButton.innerText = element.completed ? "Ha" : "Yo'q";
+        elTitle.innerText = element.name;
+        elDescription.innerText = element.description;
+        elCategory.innerText = element.category;
+        elPrice.innerText = element.price;
+        elBrand.innerText = element.brand;
 
         elContainer.append(clone);
     });
