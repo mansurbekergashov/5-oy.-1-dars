8efc8bdfec36d6d07ad35b02eb915d0ee773f931
mansurbek <ergashovmansurbek5577@gmail.com>
2025-09-10 15:07:54 +0500

    cars

--- js/app.js
+++ js/app.js
@@ -1,26 +1,51 @@
 const elCardTemplate = document.getElementById("cardTemp");
 const elContainer = document.getElementById("container");
 const elLoading = document.getElementById("loading");
+const elError = document.getElementById("error");
 
 
 
+function init() {
+    elLoading.style.display = "block";
+    fetch("https://json-api.uz/api/project/fn43/cars")
+        .then((res) => {
+            return res.json();
+        })
+        .then((res) => {
+            ui(res.data);
+        })
+        .catch(() => {
+            console.log("Xatolik");
+            elError.classList.remove("hidden");
+        })
+        .finally(() => {
+            elLoading.style.display = "none";
+        })
+}
 
-fetch("https://json-api.uz/api/project/fn43/cars")
-    .then((res) => {
-        return res.json();
-    })
-    .then((res) => {
-        ui(res.data);
-    })
-    .catch(() => {
-        console.log("Xatolik");
-    })
-    .finally(() => {
-        elLoading.remove()
-    })
+init()
+
+
+function deleteEl(id) {
+    elLoading.style.display = "block";
+    fetch(`https://json-api.uz/api/project/fn43/cars/${id}`, { method: "DELETE", })
+        .then((res) => {
+            init();
+        })
+        .then((res) => {
+        })
+        .catch(() => {
+        })
+        .finally(() => {
+        })
+}
+
+
+
 
 
 function ui(cars) {
+    elContainer.innerHTML = "";
     cars.forEach(element => {
         const clone = elCardTemplate.cloneNode(true).content;
         const elTitle = clone.querySelector("h2");
@@ -28,13 +53,25 @@ function ui(cars) {
         const elDescription = clone.querySelector("p");
         const elCategory = clone.querySelector("mark");
         const elPrice = clone.querySelector("i");
+        const elDeleteBtn = clone.querySelector(".delete-btn");
 
         elTitle.innerText = element.name;
         elDescription.innerText = element.description;
         elCategory.innerText = element.category;
         elPrice.innerText = `Narxi: $${element.price}`;
         elBrand.innerText = element.brand;
+        elDeleteBtn.id = element.id;
 
         elContainer.append(clone);
     });
 };
+
+
+
+document.addEventListener("click", (evt) => {
+    if (evt.target.classList.contains("delete-btn")) {
+        const id = evt.target.id;
+        deleteEl(id)
+    }
+});
+
